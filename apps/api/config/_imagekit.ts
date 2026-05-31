b74da19d78991aa9711b9d219f2fc665ccb78954
maryem web <34484057+MaryemChaaba@users.noreply.github.com>
2026-05-31 14:17:32 +0200

    build all doc

--- apps/api/config/_imagekit.ts
+++ apps/api/config/_imagekit.ts
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || "",
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
-  baseURL: process.env.IMAGEKIT_URL_ENDPOINT || "",
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || "",
 });
 
 export default imagekit;

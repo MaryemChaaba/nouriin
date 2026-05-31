4714d959c2fb933184ac7e23d1fddd5ebec9a181
maryem web <34484057+MaryemChaaba@users.noreply.github.com>
2026-05-31 15:56:09 +0200

    change file imagekit

--- apps/api/config/uploadService.ts
+++ apps/api/config/uploadService.ts
@@ -1,6 +1,6 @@
 import cloudinary from "./cloudinary.js";
 import { S3_CONFIG } from "./aws-s3.js";
-import imagekit from "./_imagekit.js";
+import imagekit from "./imagekit.js";
 import { PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import crypto from "crypto";
